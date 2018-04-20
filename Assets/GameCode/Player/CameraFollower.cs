f3e752a79966df594f6819e14cfee624790d33e2
Egon Elbre <egonelbre@gmail.com>
2018-04-20 22:23:58 +0300

    Add basic game controller

---
+++ Assets/GameCode/Player/CameraFollower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+    // TODO: get better camera follwer
+    public GameObject player;
+    public Vector3 offset;
+
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/GameCode/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float MaxSpeed = 10f;
+    // The fastest the player can travel in the x axis.
+    [SerializeField] private float JumpForce = 400f;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(move * MaxSpeed, body.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(new Vector2(0f, JumpForce));
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+}
