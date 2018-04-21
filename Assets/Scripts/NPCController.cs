e531edb6ec631bee204fb0e7c315f6a48e372541
Joosep Jääger <joosep.jaager@gmail.com>
2018-04-21 19:39:32 +0300

    Added enemy attacks

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -9,6 +9,9 @@ public class NPCController : MonoBehaviour {
 	public float groundCheckDistance = 0.1f;
 	public float movementSpeed = 1;
 	public float walkCheckDistance = 0.5f;
+	public float attackDistance = 1.0f;
+
+	public Animator anim;
 
 	private bool isMovingRight;
 
@@ -26,11 +29,16 @@ public class NPCController : MonoBehaviour {
 			velocity.y = 0;
 			velocity.x = movementSpeed * (isMovingRight ? 1 : -1);
 			RaycastHit2D[] walkCheck = Physics2D.CircleCastAll(transform.position, groundCheckRadius-0.1f, new Vector2(isMovingRight ? 1 : -1, 0));
-			if(walkCheck.Length > 1 && walkCheck[1].distance <= walkCheckDistance){
-				Debug.Log(walkCheck[1].collider.gameObject);
-				isMovingRight = !isMovingRight;
-				float xScale = Mathf.Abs(transform.localScale.x) * (isMovingRight ? -1 : 1);
-				transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+
+			if(walkCheck.Length > 1){
+				if(walkCheck[1].distance < attackDistance && walkCheck[1].collider.gameObject.tag == "Player"){
+					anim.Play("attack");
+				}else if(walkCheck[1].distance <= walkCheckDistance){
+					Debug.Log(walkCheck[1].collider.gameObject);
+					isMovingRight = !isMovingRight;
+					float xScale = Mathf.Abs(transform.localScale.x) * (isMovingRight ? -1 : 1);
+					transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+				}
 			}
 		}else{
 			//Debug.Log(groundCheck[1].distance);
