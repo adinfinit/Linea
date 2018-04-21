5348c2fbaf9a31bb72e6594a059e96c688853f2c
Egon Elbre <egonelbre@gmail.com>
2018-04-21 21:51:38 +0300

    Add attack triggers.

--- Assets/GameCode/Player/PlayerController.cs
+++ Assets/GameCode/Player/PlayerController.cs
@@ -14,14 +14,13 @@ public class PlayerController : MonoBehaviour
 
 	public float Gravity = 10.0f;
 
-	public Vector3 GroundCheck = new Vector3 (0, -2f, 0);
 	public float GroundRadius = 0.3f;
 	public float GroundSpread = 0.5f;
 	public LayerMask GroundLayer;
 
-	[Header ("State")]
 	private Rigidbody2D body;
-	private BoxCollider2D boxCollider;
+	private BoxCollider2D bodyCollider;
+
 	private bool grounded = false;
 	private bool hitHead = false;
 
@@ -32,40 +31,39 @@ public class PlayerController : MonoBehaviour
 	private int jumpCount = 0;
 
 	private float lastAnimationSpeed = 0.0f;
-
 	Animator animator;
 
 	void Awake ()
 	{
 		body = GetComponent<Rigidbody2D> ();
-		boxCollider = GetComponent<BoxCollider2D> ();
+		bodyCollider = GetComponent<BoxCollider2D> ();
 		animator = GetComponent<Animator> ();
 	}
 
 	Vector3 GroundCheck1 ()
 	{
-		var bounds = boxCollider.bounds;
+		var bounds = bodyCollider.bounds;
 		Vector3 center = new Vector3 (bounds.center.x + GroundSpread, bounds.min.y - GroundRadius);
 		return center;
 	}
 
 	Vector3 GroundCheck2 ()
 	{
-		var bounds = boxCollider.bounds;
+		var bounds = bodyCollider.bounds;
 		Vector3 center = new Vector3 (bounds.center.x - GroundSpread, bounds.min.y - GroundRadius);
 		return center;
 	}
 
 	Vector3 HeadCheck1 ()
 	{
-		var bounds = boxCollider.bounds;
+		var bounds = bodyCollider.bounds;
 		Vector3 center = new Vector3 (bounds.center.x + GroundSpread, bounds.max.y + GroundRadius);
 		return center;
 	}
 
 	Vector3 HeadCheck2 ()
 	{
-		var bounds = boxCollider.bounds;
+		var bounds = bodyCollider.bounds;
 		Vector3 center = new Vector3 (bounds.center.x - GroundSpread, bounds.max.y + GroundRadius);
 		return center;
 	}
@@ -163,7 +161,7 @@ public class PlayerController : MonoBehaviour
 		animator.SetFloat ("Speed", lastAnimationSpeed);
 
 		if (Input.GetButtonDown ("Fire1")) {
-			animator.Play ("Attack", -1, 0);
+			StartAttack ();
 		}
 
 		if (direction.x > 0.1f) {
@@ -177,10 +175,44 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	[Header("Attack")]
+
+	public BoxCollider2D AttackCollider;
+	private bool attacked = false;
+
+	void StartAttack ()
+	{
+		attacked = false;
+		animator.Play ("Attack", -1, 0);
+	}
+
+	void Attack ()
+	{
+		if (attacked)
+			return;
+
+		Collider2D[] hits = new Collider2D[1];
+		ContactFilter2D filter = new ContactFilter2D ();
+		filter.SetLayerMask (1 << LayerMask.NameToLayer("Enemy"));
+		Physics2D.OverlapCollider(AttackCollider, filter, hits);
+		Collider2D hit = hits [0];
+		if (hit == null)
+			return;
+		
+		NPCController enemy = hit.GetComponent<NPCController> ();
+		if (enemy == null)
+			return;
+
+		attacked = true;
+		enemy.HitByPlayer ();
+	}
+
+	#region Debug
+
 	void OnDrawGizmos ()
 	{
-		if (boxCollider == null) {
-			boxCollider = GetComponent<BoxCollider2D> ();
+		if (bodyCollider == null) {
+			bodyCollider = GetComponent<BoxCollider2D> ();
 		}
 		Gizmos.color = grounded ? Color.green : Color.blue;
 		Gizmos.DrawSphere (GroundCheck1 (), GroundRadius);
@@ -190,4 +222,6 @@ public class PlayerController : MonoBehaviour
 		Gizmos.DrawSphere (HeadCheck1 (), GroundRadius);
 		Gizmos.DrawSphere (HeadCheck2 (), GroundRadius);
 	}
+
+	#endregion
 }
