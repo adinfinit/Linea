5793be008a9a3592f2c00686bf70b07b898adb5c
Egon Elbre <egonelbre@gmail.com>
2018-04-21 22:46:24 +0300

    Add rotator script

---
+++ Assets/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+	public float AngularSpeed = 60f;
+
+	void Update ()
+	{
+		var angles = transform.localRotation.eulerAngles;
+		angles.z += AngularSpeed * Time.deltaTime;
+		transform.localRotation = Quaternion.Euler (angles);
+	}
+}
