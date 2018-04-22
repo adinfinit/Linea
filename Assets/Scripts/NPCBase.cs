ae16d76a0a0e07b793b5bcf6960f10e49cd7a86d
Joosep Jääger <joosep.jaager@gmail.com>
2018-04-22 11:57:25 +0300

    Added target field to pick text script

---
+++ Assets/Scripts/NPCBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NPCBase : MonoBehaviour {
+
+	public abstract void SetMovementEnabled(bool enabled);
+	public abstract bool GetMovementEnabled();
+
+}
--- Assets/UI/PickTextController.cs
+++ Assets/UI/PickTextController.cs
@@ -8,23 +8,34 @@ public class PickTextController : MonoBehaviour {
 
 	public GameObject mercyText, danceText;
 	public Transform arrow;
+	public NPCBase target;
+
+	private bool waitingForChoice = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		target.SetMovementEnabled(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float phase = Mathf.Sin(Mathf.PI * Time.time);
-		arrow.localScale = new Vector3(Mathf.Sign(phase) * (Mathf.Abs(phase/2)+0.5f), Mathf.Abs(phase/2)+0.5f, 1);
-		mercyText.transform.localScale = Mathf.Lerp(0.5f, 1, (phase+1)/2) * Vector3.one;
-		danceText.transform.localScale = Mathf.Lerp(0.5f, 1, (-phase+1)/2) * Vector3.one;
+		if(waitingForChoice){
+			float phase = Mathf.Sin(Mathf.PI * Time.time);
+			arrow.localScale = new Vector3(Mathf.Sign(phase) * (Mathf.Abs(phase/2)+0.5f), Mathf.Abs(phase/2)+0.5f, 1);
+			mercyText.transform.localScale = Mathf.Lerp(0.5f, 1, (phase+1)/2) * Vector3.one;
+			danceText.transform.localScale = Mathf.Lerp(0.5f, 1, (-phase+1)/2) * Vector3.one;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.tag == "Player"){
-			Debug.Log ("Choice trigger entered");
+			PlayerController pc = other.gameObject.GetComponent<PlayerController>();
+			pc.enabled = false;
+			other.GetComponentInChildren<Animator>().SetFloat("Speed", 0);
+
+			mercyText.SetActive(true);
+			danceText.SetActive(true);
+			arrow.gameObject.SetActive(true);
 		}
 	}
 }
