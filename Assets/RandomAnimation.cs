3791b87ea999a96f889dc0b825e03bfbc7852928
Egon Elbre <egonelbre@gmail.com>
2018-04-22 11:54:19 +0300

    Add plant prefab

---
+++ Assets/RandomAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimation : MonoBehaviour
+{
+	private Animator animator;
+
+	void Start ()
+	{
+		animator = GetComponentInChildren<Animator> ();
+		animator.SetFloat ("Random", Random.Range (0.0f, 1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
